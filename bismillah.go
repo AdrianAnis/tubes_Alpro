fb3da8d7e2bbcb60c33816f0e9b3147948419f5a
AdrianAnis <adriananis06@gmail.com>
2025-05-19 15:16:29 +0700

    Penambahan Fitur
    Menambahkan fitur agar dapat menambahkan tim

--- bismillah.go
+++ bismillah.go
@@ -4,46 +4,46 @@ import "fmt"
 
 const NMAX int = 100000
 
-type DataUtamaEsport struct {
-	daftarTim          string
-	jadwalPertandingan int
-	hasilPertandingan  int
+type dataTimTournament struct {
+	namaTim   string
+	jadwalTim string
+	win       int
+	lose      int
+	skorTim   int
 }
-type DataUtama [NMAX]DataUtamaEsport
+type dataTour [NMAX]dataTimTournament
+
+var jumTimTour int
+var dataTim dataTour
+
+var timA, timB, JadwalMatch string
+var skorA, skorB int
+var namaTim int
+
+var jumHasilPertandingan int
+
+//========================================================================================================================================================================//
 
 func main() {
-	var namaTim DataUtama
-	var pilihOpsiPeran, pilihOpsiMenuPanitia, pilihOpsiMenuPeserta,n int
+	var pilihOpsiPeranFunc int
+	var pilihOpsiMenuPanitia int
+	var pilihOpsiMenuPeserta int
+	var n int
 
-	MenuUtama(&pilihOpsiPeran)
-	if pilihOpsiPeran == 1 {
+	MenuUtama(&pilihOpsiPeranFunc)
+	if pilihOpsiPeranFunc == 1 {
 		menuPanitia(&pilihOpsiMenuPanitia)
 		switch pilihOpsiMenuPanitia {
 		case 1:
-			menambahData(&n,&namaTim)
-			jadwalPertandingan(&namaTim)
-		case 2:
-
-		case 4:
-
-		case 5:
-
-		case 6:
-			MenuUtama(&pilihOpsiPeran)
+			nambahJumTim(n)
+			masukanHasilPertandingan()
 		}
-	} else if pilihOpsiPeran == 2 {
+	} else if pilihOpsiMenuPanitia == 2 {
 		menuPeserta(&pilihOpsiMenuPeserta)
-		switch pilihOpsiMenuPeserta {
-		case 1:
-
-		case 2:
-
-		case 3:
-
-		}
 	}
-
 }
+
+//========================================================================================================================================================================//
 
 func MenuUtama(pilihOpsiPeranFunc *int) {
 	for *pilihOpsiPeranFunc != 1 && *pilihOpsiPeranFunc != 2 {
@@ -87,7 +87,8 @@ func menuPanitia(pilihOpsiMenuPanitia *int) {
 		fmt.Print("\t║ 3. Menghapus Data TIM                            ║\t\n")
 		fmt.Print("\t║ 4. Mencari TIM                                   ║\t\n")
 		fmt.Print("\t║ 5. Mengurutkan TIM (Berdasarkan Poin )           ║\t\n")
-		fmt.Print("\t║ 6. Halaman Utama                                 ║\t\n")
+		fmt.Print("\t║ 6. Klasemen                                      ║\t\n")
+		fmt.Print("\t║ 7. Halaman Utama                                 ║\t\n")
 		fmt.Print("\t║                                                  ║\t\n")
 		fmt.Print("\t╚══════════════════════════════════════════════════╝\t\n")
 		fmt.Print("\tMasukkan Pilihan Anda: ")
@@ -124,19 +125,17 @@ func menuPeserta(pilihOpsiMenuPeserta *int) {
 	}
 }
 
-func menambahData(n *int, namaTim *DataUtama) {
+//========================================================================================================================================================================//
+func nambahJumTim(n int) {
 
-	masukanJumlahTim(n)
-	masukanNamaTim(*n, namaTim)
-}
-func masukanJumlahTim(n *int) {
 	fmt.Print("\n\t╔══════════════════════════════════════════════════╗\t\n")
-	fmt.Print("\t║    Berapa total TIM yang akan berpartisipasi?    ║\t\n")
+	fmt.Print("\t║      Berapa total TIM yang akan Ditambahkan?     ║\t\n")
 	fmt.Print("\t╚══════════════════════════════════════════════════╝\t\n")
 	fmt.Print("\tMasukkan Pilihan Anda: ")
-	fmt.Scan(n)
+	fmt.Scan(&n)
+	masukanTimTanggal(n)
 }
-func masukanNamaTim(n int, namaTim *DataUtama) {
+func masukanTimTanggal(n int) {
 	var i int
 
 	fmt.Print("\n\t╔══════════════════════════════════════════════════╗\t\n")
@@ -144,52 +143,117 @@ func masukanNamaTim(n int, namaTim *DataUtama) {
 	fmt.Print("\t║    dan Jadwal pertandingan Dari TIM tersebut     ║\t\n")
 	fmt.Print("\t╚══════════════════════════════════════════════════╝\t\n")
 
-	for i = 0; i < n; i++ {
-		fmt.Printf("\tMasukkan nama tim ke-%d: ", i+1)
-		fmt.Scan(&namaTim[i].daftarTim)
+	for i = 1; i <= n; i++ {
+		fmt.Printf("\tMasukkan nama tim ke-%d: ", i)
+		fmt.Scan(&dataTim[jumTimTour].namaTim)
 
-		fmt.Printf("\tMasukkan jadwal pertandingan tim %s: ",namaTim[i].daftarTim)
-		fmt.Scan(&namaTim[i].jadwalPertandingan)
+		fmt.Printf("\tMasukkan jadwal pertandingan tim %s: ", dataTim[jumTimTour].namaTim)
+		fmt.Scan(&dataTim[jumTimTour].jadwalTim)
+		jumTimTour++
 	}
 }
-func jadwalPertandingan(namaTim *DataUtama){
-	var timA,timB string
-	var jadwal,n int
-	masukanPertandingan(*namaTim,&timA,&timB,&jadwal)
-	cekTimDanJadwal(timA,timB,jadwal,namaTim,n)
-}
-func masukanPertandingan(namaTim DataUtama, timA,timB *string, jadwal *int){
+func masukanPertandingan() {
 	fmt.Print("\n\t╔══════════════════════════════════════════════════╗\t\n")
 	fmt.Print("\t║      Masukkan Nama Tim yang akan bertanding      ║\t\n")
 	fmt.Print("\t║         dan Jadwal pertandingan Hari ini         ║\t\n")
 	fmt.Print("\t╚══════════════════════════════════════════════════╝\t\n")
 
 	fmt.Print("\t TIM A: ")
-	fmt.Scan(timA)
+	fmt.Scan(&timA)
 	fmt.Print("\t TIM B: ")
-	fmt.Scan(timB)
+	fmt.Scan(&timB)
 	fmt.Print("\t Jadwal Pertandingan Hari ini: ")
-	fmt.Scan(jadwal)
+	fmt.Scan(&JadwalMatch)
 }
-func cekTimDanJadwal(timA, timB string, jadwal int, namaTim *DataUtama, n int) {
+func masukanHasilPertandingan() {
+	var i int
 	var ditemukanA, ditemukanB bool
+	var idxTimA, idxTimB int
 
-	for i := 0; i < n; i++ {
-		if namaTim[i].daftarTim == timA && namaTim[i].jadwalPertandingan == jadwal {
+	masukanPertandingan()
+
+	for i = 0; i < jumTimTour; i++ {
+		if dataTim[i].namaTim == timA && dataTim[i].jadwalTim == JadwalMatch {
 			ditemukanA = true
 		}
-		if namaTim[i].daftarTim == timB && namaTim[i].jadwalPertandingan == jadwal {
+		if dataTim[i].namaTim == timB && dataTim[i].jadwalTim == JadwalMatch {
 			ditemukanB = true
 		}
 	}
 	if ditemukanA && ditemukanB {
-		fmt.Println("\n Kedua tim ditemukan dan memiliki jadwal yang sama.")
-	} else if !ditemukanA && !ditemukanB {
-		fmt.Println("\n Kedua tim tidak ditemukan atau jadwalnya salah.")
-	} else if !ditemukanA {
-		fmt.Println("\n Tim A tidak ditemukan atau jadwalnya salah.")
+		idxTimA = cariIdxTim(timA)
+		idxTimB = cariIdxTim(timB)
+
+		masukanSkor()
+
+		dataTim[idxTimA].win = skorA
+		dataTim[idxTimA].lose = skorB
+
+		dataTim[idxTimB].lose = skorA
+		dataTim[idxTimB].win = skorB
+
+		if skorA > skorB {
+			dataTim[idxTimA].skorTim = dataTim[idxTimA].skorTim + 3
+		} else if skorB > skorA {
+			dataTim[idxTimB].skorTim = dataTim[idxTimA].skorTim + 3
+		}
+	} else if ditemukanA && !ditemukanB {
+		fmt.Println("\n\tTim B tidak ditemukan atau jadwalnya salah.")
+	} else if !ditemukanA && ditemukanB {
+		fmt.Println("\n\tTim A tidak ditemukan atau jadwalnya salah.")
 	} else {
-		fmt.Println("\nRESAN GACOR")
+		fmt.Println("\n\tKedua tim tidak ditemukan atau jadwalnya salah.")
+	}
+}
+func cariIdxTim(tim string) int {
+	var ketemu int
+	var k int
+
+	ketemu = -1
+	k = 0
+	for ketemu == -1 && k < jumTimTour {
+		if dataTim[k].namaTim == tim {
+			ketemu = k
+		}
+		k++
+	}
+	return ketemu
+}
+func masukanSkor() {
+	fmt.Printf("\t Masukkan Skor Tim %s: ", timA)
+	fmt.Scan(&skorA)
+	fmt.Printf("\t Masukkan Skor Tim %s: ", timB)
+	fmt.Scan(&skorB)
+}
+
+//========================================================================================================================================================================//
+
+func ubahDataTim() {
+	var pilihOpsiUbah int
+	pilihUbah(&pilihOpsiUbah)
+	switch pilihOpsiUbah{
+	case 1:
+
 	}
 }
 
+func pilihUbah(pilihOpsiUbah *int) {
+	fmt.Print("\n\t╔══════════════════════════════════════════════════╗\t\n")
+	fmt.Print("\t║                                                  ║\t\n")
+	fmt.Print("\t║         MASUKKAN DATA APA YANG INGIN DIUBAH      ║\t\n")
+	fmt.Print("\t║                                                  ║\t\n")
+	fmt.Print("\t║==================================================║\t\n")
+	fmt.Print("\t║ Silahkan Pilih Opsi Yang Ada Dibawah             ║\t\n")
+	fmt.Print("\t║                                                  ║\t\n")
+	fmt.Print("\t║ 1. Nama TIM                                      ║\t\n")
+	fmt.Print("\t║ 2. Jadwal TIM                                    ║\t\n")
+	fmt.Print("\t║ 3. Hasil Pertandingan                            ║\t\n")
+	fmt.Print("\t║                                                  ║\t\n")
+	fmt.Print("\t╚══════════════════════════════════════════════════╝\t\n")
+	fmt.Print("\tMasukkan Pilihan Anda: ")
+	fmt.Scan(pilihOpsiUbah)
+}
+func cekUbahData(){
+	fmt.Print("\n\t Masukkan Nama Tim yang Ingin Anda Ubah: ")
+	fmt.Print(namaTim)
+}
